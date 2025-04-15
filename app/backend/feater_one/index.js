d76a0bc74bc6af82197668ab1a84726961f4b28d
Debanjan Das <microsoft2420@gmail.com>
2025-04-15 18:38:12 +0530

    mongoDB connection failed

--- app/backend/feater_one/index.js
+++ app/backend/feater_one/index.js
@@ -1,1 +1,20 @@
 // Featur one will be maintained by Debanjan it leads us to the sign in process 
+import express from "express" ;
+import { connectDb } from "./db/connection.db.js"
+
+const port = process.env.PORT || 3000 ;
+
+const app = express() ;
+
+connectDb()
+.then(() => {
+    app.on('error' , (err) => {
+        console.log(`App crashed ! ${err}`);
+        throw new err ;
+    })
+    app.listen(port , () => {
+        console.log(`App listening at http://localhost:${port}`);
+    })
+    
+})
+
