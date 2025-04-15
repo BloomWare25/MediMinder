bc2977092fdebfc306dc357f55878e693f62cf6d
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-15 17:48:52 +0530

    modified:   app/backend/feature_two/main.py

--- app/backend/feature_two/main.py
+++ app/backend/feature_two/main.py
@@ -1,2 +1,25 @@
 # feature two will be maintained by Romyojit this will lead to the medication status 
-# test push
+from dotenv import load_dotenv, dotenv_values # For getting env variables
+from pymongo import MongoClient # For connecting to MongoDB Atlas
+import os
+
+load_dotenv(override=False) # Load environment variables from .env file
+
+uri = os.getenv("MONGO_ATLAS_STRING") # Get the MongoDB connection string from environment variables
+
+client = MongoClient(uri) # Create a MongoClient instance to connect to MongoDB Atlas
+
+print(client.list_database_names()) # Print the names of all databases in the MongoDB instance
+
+db = client["sample_mflix"] # Connect to the "sample_mflix" database
+
+print(db.list_collection_names()) # Print the names of all collections in the database
+
+collection = db["embedded_movies"] # Connect to the "embedded_movies" collection in the database
+
+print(collection.find_one()['fullplot']) # Print one document from the collection and its 'fullplot' field
+for c in client.list_database_names():
+    print(c)
+
+db = client["sample_mflix"] # Connect to the "sample_mflix" database
+collection = db["embedded_movies"] # Connect to the "embedded_movies" collection in the database
