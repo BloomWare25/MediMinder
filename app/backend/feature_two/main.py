acef27c063cc0ae22f7bfef239079ec50401ac7f
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-15 17:19:26 +0530

    modified:   .gitignore 	modified:   app/backend/feature_two/main.py

--- app/backend/feature_two/main.py
+++ app/backend/feature_two/main.py
@@ -1,1 +1,8 @@
 # feature two will be maintained by Romyojit this will lead to the medication status 
+from dotenv import load_dotenv, dotenv_values # For getting env variables
+from pymongo import MongoClient # For connecting to MongoDB Atlas
+import os
+
+load_dotenv()
+
+print(os.getenv("MONGO_ATLAS_STRING"))
