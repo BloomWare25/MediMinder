c623f652daae18aba8e8b6ee1730eb2d305fe0cd
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-19 16:48:31 +0530

    modified:   .gitignore 	modified:   app/backend/feature_two/main.py

--- app/backend/feature_two/main.py
+++ app/backend/feature_two/main.py
@@ -1,8 +1,22 @@
 # feature two will be maintained by Romyojit this will lead to the medication status 
 from dotenv import load_dotenv # For getting env variables
 from pymongo import MongoClient # For connecting to MongoDB Atlas
+from pydantic import BaseModel # For creating data models
+from fastapi import FastAPI # For creating the FastAPI application
 import os
 import time
+
+app = FastAPI()
+
+class Medication(BaseModel):
+    userId: str
+    medicineName: str
+    dosage: str
+    intakeTime: str
+    notes: str
+    created_at: str = time.asctime(time.localtime(time.time()))
+    updated_at: str = created_at
+
 
 load_dotenv(override=False) # Load environment variables from .env file
 
@@ -16,19 +30,24 @@ db = client["sample_mflix"] # Connect to the "sample_mflix" database
 
 print(db.list_collection_names()) # Print the names of all collections in the database
 
-collection = db["users"] # Connect to the "users" collection in the database
+collection = db["medications"] # Connect to the "medications" collection in the database
 
-post = {
-    '_id': 1,
-    'name': 'Romyojit',
-    "email": "test@gmail.com",
-    "password": "test123",
-    "updated_at": time.asctime(time.localtime(time.time())),
-}
-collection.update_one({'_id': 1}, {'$set': post}) # Update the document with _id 1 in the "users" collection
+all = collection.find({}) # Find all documents in the "medications" collection
+
+@app.get("/")
+def first_example():
+    med = Medication(
+        userId="Romyojit",
+        medicineName="paracetamol",
+        dosage="500mg",
+        intakeTime="Two times a day.",
+        notes="Take with water.",
+    ) # Create a new Medication object
+    
+    collection.insert_one(med.model_dump()) # Create the document with _id in the "medications" collection
+    return {"message": "Medication added successfully!"} # Return a success message
 
 
-all = collection.find({}) # Find all documents in the "users" collection
 for res in all:
     print(res) # Print each document in the collection
 
