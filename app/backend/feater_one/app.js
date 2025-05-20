f40e4421735d3b626cde2570364aea22dae6cef5
Debanjan Das <microsoft2420@gmail.com>
2025-05-20 10:50:41 +0530

    fix: remove rate limiting middleware from app configuration

--- app/backend/feater_one/app.js
+++ app/backend/feater_one/app.js
@@ -1,15 +1,8 @@
 import express from "express";
 import { router } from "./routes/index.js"
 import { updateTokenRouter } from "./routes/updateToken.route.js"
-import rateLimit from "express-rate-limit" 
 const app = express() ;
 
-
-const limitedReq = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limitedReq);
 app.use(express.json(
     {
         limit: '20kb',
